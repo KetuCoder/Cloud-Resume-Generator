74ddeab22d4b085b937fbdc399942fd117a64383
KetuCoder <ambureketan@gmail.com>
2025-10-29 11:25:35 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link , useNavigate  } from "react-router-dom";
 
 export default function Navbar() {
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("resumeData");
+    localStorage.removeItem("user");
+    navigate("/");
+  };
+
   return (
     <nav>
       <h1>Cloud Based Resume Generator</h1>
       <div>
         <Link to="/dashboard">Dashboard</Link>
         <Link to="/resume">Resumes</Link>
-        <Link to="/">Logout</Link>
+        <Link to="/" onClick={handleLogout}>Logout</Link>
       </div>
     </nav>
   );
