2384d20a1721b3c5293d9d4fad4fcf945375bf4d
Ketan Ambure <ambureketan@gmail.com>
2025-11-09 18:23:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,9 @@ export default defineConfig({
   plugins: [react(),tailwindcss()],
   server : {
     port : 3000
+  hmr : {
+    host: '3.79.228.239',  // public IP of your node
+    port: 30080 
+    }
   }
 })
